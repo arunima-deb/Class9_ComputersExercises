fadce1bb7577cf771ce879d1ac8d151bc666b398
Arunima Deb <arunima.s.deb@gmail.com>
2023-09-22 23:48:49 +0530

    Ch 7 Conditionals; Exercise 5 q6

---
+++ src/com/arunima/class9/ch7/Exercise5_6.java
@@ -0,0 +1,54 @@
+package com.arunima.class9.ch7;
+
+import java.util.* ;
+
+
+
+/* Write a program to input two unequal positive numbers and check
+ * whether they are perfect squares or not. If the user enters any 
+ * negative number them the program displays the message "Square
+ * root of a positive number cannot be determined."
+ */
+public class Exercise5_6 {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in) ;
+		System.out.println( "Enter two unequal positive numbers : " ) ;
+		double a = sc.nextDouble() ;
+		double b = sc.nextDouble();
+		
+		if(a==b)
+		{
+			System.out.println( "Both numbers are equal." );
+		}
+		else if(a<0||b<0)
+		{
+			System.out.println( "Square root of a negative number does not exist." ) ;
+		}
+		else 
+		{
+			
+			if ( Math.sqrt(a)==Math.floor(Math.sqrt(a)) )
+			{
+				if( Math.sqrt(b)==Math.floor(Math.sqrt(b)) )
+				{
+					System.out.println( "Both " + a + " and " + b + "are perfect squares." ) ;
+				}
+			}
+			else if (Math.sqrt(b)==Math.floor(Math.sqrt(b)))
+			{
+				System.out.println( a + " is not a perfect square." ) ;
+				System.out.println( b + " is a perfect square." ) ;
+			}
+			else 
+			{
+				System.out.println( "Neither of the numbers are perfect squares." ) ;
+			}
+			
+		}
+		
+		sc.close() ;
+	}
+
+}
