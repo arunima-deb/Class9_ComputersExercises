969f1dbfb0007d0fd46b20deccdca193854b6217
Arunima Deb <arunima.s.deb@gmail.com>
2023-09-23 11:42:25 +0530

    Ch 7 Exercise 5 q6 improved logic

--- src/com/arunima/class9/ch7/Exercise5_6.java
+++ src/com/arunima/class9/ch7/Exercise5_6.java
@@ -2,7 +2,8 @@ package com.arunima.class9.ch7;
 
 import java.util.* ;
 
-/* Write a program to input two unequal positive numbers and check
+/**
+ * Write a program to input two unequal positive numbers and check
  * whether they are perfect squares or not. If the user enters any 
  * negative number them the program displays the message "Square
  * root of a positive number cannot be determined."
@@ -16,34 +17,21 @@ public class Exercise5_6 {
 		double a = sc.nextDouble() ;
 		double b = sc.nextDouble() ;
 		
-		if(a==b)
+		if( a==b )
 		{
 			System.out.println( "Both numbers are equal." );
 		}
-		else if(a<0||b<0)
+		else if( a<0 || b<0 )
 		{
 			System.out.println( "Square root of a negative number does not exist." ) ;
 		}
 		else 
 		{
+			boolean aIsPerfectSq = Math.sqrt(a)==Math.floor(Math.sqrt(a)) ;
+			boolean bIsPerfectSq = Math.sqrt(b)==Math.floor(Math.sqrt(b)) ;
 			
-			if ( Math.sqrt(a)==Math.floor(Math.sqrt(a)) )
-			{
-				if( Math.sqrt(b)==Math.floor(Math.sqrt(b)) )
-				{
-					System.out.println( "Both " + a + " and " + b + "are perfect squares." ) ;
-				}
-			}
-			else if (Math.sqrt(b)==Math.floor(Math.sqrt(b)))
-			{
-				System.out.println( a + " is not a perfect square." ) ;
-				System.out.println( b + " is a perfect square." ) ;
-			}
-			else 
-			{
-				System.out.println( "Neither of the numbers are perfect squares." ) ;
-			}
-			
+			System.out.println( a + " is" + ((aIsPerfectSq) ? " " : " not ") + "a perfect square" ) ;
+			System.out.println( b + " is" + ((bIsPerfectSq) ? " " : " not ") + "a perfect square" ) ;
 		}
 		
 		sc.close() ;
